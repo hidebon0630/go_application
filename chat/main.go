a1815ea47cbb91803fb83fe0eb3f33074388464e
hidebon0630 <hyagi0630@icloud.com>
2021-09-05 19:21:20 +0900

    chapter 2.5

--- chat/main.go
+++ chat/main.go
@@ -30,16 +30,21 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func main() {
 	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
 	flag.Parse() // フラグを解釈します
+
 	r := newRoom()
 	r.tracer = trace.New(os.Stdout)
+
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
 	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/room", r)
+
 	// チャットルームを開始します
 	go r.run()
+
 	// Webサーバーを起動します
 	log.Println("Webサーバーを開始します。ポート: ", *addr)
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
 }
