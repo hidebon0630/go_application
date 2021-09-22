3d14f02d65b9deca4ade5615147697d98ba0607b
hidebon0630 <hyagi0630@icloud.com>
2021-09-22 16:05:43 +0900

    chapter 8.3.2

--- backup/cmds/backup/main.go
+++ backup/cmds/backup/main.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"flag"
 	"log"
+	"strings"
 
 	"github.com/matryer/filedb"
 )
@@ -41,5 +42,11 @@ func main() {
 	if err != nil {
 		fatalErr = err
 		return
+	}
+
+	switch strings.ToLower(args[0]) {
+	case "list":
+	case "add":
+	case "remove":
 	}
 }
